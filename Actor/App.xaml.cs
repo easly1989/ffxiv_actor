87352b83a1e9413cfd9b37c685679306cd9709d2
easly1989 <caesar.themaster@gmail.com>
2018-01-29 10:05:37 +0100

    Removed StartupUri

--- Actor/App.xaml.cs
+++ Actor/App.xaml.cs
@@ -1,6 +1,14 @@
-﻿namespace Actor
+﻿using System.Windows;
+
+namespace Actor
 {
     public partial class App
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+
+        }
     }
 }
