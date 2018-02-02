e5a9691338b147f7d54275a2d7bedbe7a4a1f422
easly1989 <caesar.themaster@gmail.com>
2018-02-02 13:18:18 +0100

    Started Implementation for Act configuration before first start (wip) Added most of the configuration files

---
+++ Actor.Core/ActConfigurationHelper.cs
@@ -0,0 +1,36 @@
+﻿namespace Actor.Core
+{
+    public static class ActConfigurationHelper
+    {
+        private const string ActConfigurationFile = "";
+
+        /// <summary>
+        /// Handles the plugin in the ACT configuration
+        /// </summary>
+        /// <param name="pluginPath">The path where the plugin is installed</param>
+        /// <param name="isEnabled">Default is true, enables the plugin inside ACT</param>
+        public static void AddPlugin(string pluginPath, bool isEnabled = true)
+        {
+
+        }
+
+        /// <summary>
+        /// Saves the configuration for ACT
+        /// </summary>
+        /// <param name="overwrite">Default is true, will overwrite any existing configuration</param>
+        public static void SaveConfiguration(bool overwrite = true)
+        {
+
+        }
+
+        /// <summary>
+        /// Saves the configuration for this plugin
+        /// </summary>
+        /// <param name="from">The url to the configuration</param>
+        /// <param name="overwrite">Default is true, will overwrite any existing configuration</param>
+        public static void SavePluginConfiguration(string from, bool overwrite = true)
+        {
+
+        }
+    }
+}
