5c9ddeed711f112416b23757335d2c5aa1f6c553
easly1989 <caesar.themaster@gmail.com>
2018-02-15 13:05:28 +0100

    Simplified CleanUp on Application exit

--- ActorGui/App.xaml.cs
+++ ActorGui/App.xaml.cs
@@ -9,42 +9,37 @@ namespace ActorGui
 {
     public partial class App
     {
+        private IDisposable _disposable;
         private MainViewModel _mainViewModel;
-        private CompositeDisposable _disposables;
 
         protected override void OnStartup(StartupEventArgs e)
         {
             base.OnStartup(e);
 
-            _disposables = new CompositeDisposable();
             _mainViewModel = new MainViewModel();
 
             MainWindow = new MainWindow(_mainViewModel);
 
-            _disposables.Add(Observable.FromEventPattern<CancelEventHandler, CancelEventArgs>(
-                    handler => MainWindow.Closing += handler,
-                    handler => MainWindow.Closing -= handler)
-                .Select(result => result.EventArgs)
-                .Subscribe(_ => OnMainWindowClosing()));
-
-            _disposables.Add(Observable.FromEventPattern<UnhandledExceptionEventHandler, UnhandledExceptionEventArgs>(
+            _disposable = Observable.FromEventPattern<UnhandledExceptionEventHandler, UnhandledExceptionEventArgs>(
                     handler => AppDomain.CurrentDomain.UnhandledException += handler,
                     handler => AppDomain.CurrentDomain.UnhandledException -= handler)
                 .Select(result => result.EventArgs)
-                .Subscribe(OnUnhandledException));
+                .Subscribe(OnUnhandledException);
 
             MainWindow?.Show();
         }
 
+        protected override void OnExit(ExitEventArgs e)
+        {
+            base.OnExit(e);
+
+            _mainViewModel.Dispose();
+            _disposable.Dispose();
+        }
+        
         private void OnUnhandledException(UnhandledExceptionEventArgs args)
         {
             // todo: Handle exceptions
-        }
-
-        private void OnMainWindowClosing()
-        {
-            _mainViewModel.Dispose();
-            _disposables.Dispose();
         }
     }
 }
