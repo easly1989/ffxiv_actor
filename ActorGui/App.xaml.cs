9753f2bb1d7cf1dc14f650a4bcb637fa94e6dd7c
easly1989 <caesar.themaster@gmail.com>
2018-02-15 12:26:12 +0100

    Removed Unused references from files Moved some MainWindow startup informations from xaml to cs

--- ActorGui/App.xaml.cs
+++ ActorGui/App.xaml.cs
@@ -1,17 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 
 namespace ActorGui
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
-    public partial class App : Application
+    public partial class App
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            MainWindow = new MainWindow();
+            MainWindow.Show();
+        }
     }
 }
