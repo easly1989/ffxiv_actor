a03ec1f9cd8acde5340eab28178641f535a6dbc9
easly1989 <caesar.themaster@gmail.com>
2019-07-18 16:12:38 +0200

    Wizard first implementation - Updatated nuget dependecies

---
+++ ActorWizard/Converters/RatioConverter.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace ActorWizard.Converters
+{
+    [ValueConversion(typeof(double), typeof(string))]
+    public class RatioConverter : ValueConverterBase<double, string>
+    {
+        private static RatioConverter _instance;
+
+        protected override ValueConverterBase<double, string> OnProvideValue(IServiceProvider serviceProvider)
+        {
+            return _instance ?? (_instance = new RatioConverter());
+        }
+
+        protected override string OnConvert(double value, string parameter, CultureInfo culture)
+        {
+            try
+            {
+                var actualParameter = System.Convert.ToDouble(parameter, CultureInfo.InvariantCulture);
+                var result = Math.Abs(actualParameter) > 0 ? value * actualParameter : value;
+                return result.ToString("G0", CultureInfo.InvariantCulture);
+            }
+            catch (Exception)
+            {
+                return default(string);
+            }
+        }
+
+        protected override double OnConvertBack(string value, string parameter, CultureInfo culture)
+        {
+            try
+            {
+                var doubleValue = System.Convert.ToDouble(value, CultureInfo.InvariantCulture);
+                var actualParameter = System.Convert.ToDouble(parameter, CultureInfo.InvariantCulture);
+                var result = Math.Abs(actualParameter) > 0 ? doubleValue / actualParameter : doubleValue;
+                return result;
+            }
+            catch (Exception)
+            {
+                return default(double);
+            }
+        }
+    }
+}
---
+++ ActorWizard/Converters/ValueConverterBase.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Markup;
+
+namespace ActorWizard.Converters
+{
+    public abstract class ValueConverterBase<TValue, TTarget> : MarkupExtension, IValueConverter
+    {
+        public sealed override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return OnProvideValue(serviceProvider);
+        }
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            ParamsActualization<TValue>(value, parameter, out var actualValue, out var actualParameter);
+            return OnConvert(actualValue, actualParameter, culture);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            ParamsActualization<TTarget>(value, parameter, out var actualValue, out var actualParameter);
+            return OnConvertBack(actualValue, actualParameter, culture);
+        }
+
+        protected abstract ValueConverterBase<TValue, TTarget> OnProvideValue(IServiceProvider serviceProvider);
+        protected abstract TTarget OnConvert(TValue value, string parameter, CultureInfo culture);
+        protected abstract TValue OnConvertBack(TTarget value, string parameter, CultureInfo culture);
+
+        private static void ParamsActualization<T>(object value, object parameter, out T actualValue, out string actualParameter)
+        {
+            try
+            {
+                // ReSharper disable MergeCastWithTypeCheck
+                actualValue = value is T ? (T)value : default(T);
+                actualParameter = parameter as string;
+                // ReSharper restore MergeCastWithTypeCheck
+            }
+            catch (Exception)
+            {
+                actualValue = default(T);
+                actualParameter = default(string);
+            }
+        }
+    }
+}
