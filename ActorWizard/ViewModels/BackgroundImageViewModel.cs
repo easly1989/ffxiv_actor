6a1e8289cfcecd52c2e92a737634f5a32531d297
easly1989 <caesar.themaster@gmail.com>
2019-07-19 11:33:31 +0200

    Added BackgroundImageViewModel - Tested in the MainViewModel the transition

---
+++ ActorWizard/ViewModels/BackgroundImageViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Windows.Media.Imaging;
+using Actor.UI.Common;
+
+namespace ActorWizard.ViewModels
+{
+    public class BackgroundImageViewModel : ViewModelBase
+    {
+        public BitmapImage Source { get; }
+
+        public BackgroundImageViewModel(string absoluteUriPath)
+        {
+            Source = new BitmapImage();
+            Source.BeginInit();
+            Source.UriSource = new Uri(absoluteUriPath, UriKind.Absolute);
+            Source.EndInit();
+        }
+    }
+}
--- ActorWizard/ViewModels/MainViewModel.cs
+++ ActorWizard/ViewModels/MainViewModel.cs
@@ -1,4 +1,6 @@
-﻿using System.Reflection;
+﻿using System;
+using System.Reactive.Linq;
+using System.Reflection;
 using Actor.UI.Common;
 
 namespace ActorWizard.ViewModels
@@ -6,5 +8,34 @@ namespace ActorWizard.ViewModels
     public class MainViewModel : ViewModelBase
     {
         public string WindowTitle => $"Actor Wizard ~ v{Assembly.GetExecutingAssembly().GetName().Version}";
+
+        public BackgroundImageViewModel BackgroundImage
+        {
+            get => _backgroundImage;
+            set => Set(ref _backgroundImage, value);
+        }
+
+        private int _deleteMe;
+        public MainViewModel()
+        {
+            _deleteMe = 0;
+            Observable.Interval(TimeSpan.FromSeconds(5)).Subscribe(_ =>
+            {
+                BackgroundImage?.Dispose();
+
+                PostOnUI(__ =>
+                {
+                    BackgroundImage = _deleteMe % 2 == 0
+                        ? new BackgroundImageViewModel("https://i.pinimg.com/originals/b9/fa/05/b9fa05f7aa2e9ded35d8e29ea79557a3.jpg") 
+                        : new BackgroundImageViewModel("https://wallpaperplay.com/walls/full/c/e/4/174967.jpg");
+                });
+                
+                _deleteMe++;
+            });
+        }
+
+        #region Private Fields
+        private BackgroundImageViewModel _backgroundImage;
+        #endregion
     }
 }
