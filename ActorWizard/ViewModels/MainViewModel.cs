3584f54fbf86ca1521f2d8cee814866b1f15a267
easly1989 <caesar.themaster@gmail.com>
2019-07-22 15:06:49 +0200

    Added first implementation of Steps viewmodel

--- ActorWizard/ViewModels/MainViewModel.cs
+++ ActorWizard/ViewModels/MainViewModel.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Reactive.Linq;
 using System.Reflection;
+using System.Windows.Input;
 using Actor.UI.Common;
+using ActorWizard.ViewModels.Steps;
 
 namespace ActorWizard.ViewModels
 {
@@ -9,33 +11,58 @@ namespace ActorWizard.ViewModels
     {
         public string WindowTitle => $"Actor Wizard ~ v{Assembly.GetExecutingAssembly().GetName().Version}";
 
+        public ICommand NextCommand { get; }
+        public ICommand PreviousCommand { get; }
+        public ICommand SkipCommnad { get; }
+
+        public string NextCommandText { get; }
+        public string PreviousCommandText { get; }
+        public string SkipCommandText { get; }
+
+        public bool ShowNextCommand
+        {
+            get => _showNextCommand;
+            set => Set(ref _showNextCommand, value);
+        }
+
+        public bool ShowPreviousCommand
+        {
+            get => _showPreviousCommand;
+            set => Set(ref _showPreviousCommand, value);
+        }
+
+        public bool ShowSkipCommand
+        {
+            get => _showSkipCommand;
+            set => Set(ref _showSkipCommand, value);
+        }
+
+        public StepViewModelBase SelectedStep
+        {
+            get => _selectedStep;
+            set => Set(ref _selectedStep, value);
+        }
+
         public BackgroundImageViewModel BackgroundImage
         {
             get => _backgroundImage;
             set => Set(ref _backgroundImage, value);
         }
 
-        private int _deleteMe;
         public MainViewModel()
         {
-            _deleteMe = 0;
-            Observable.Interval(TimeSpan.FromSeconds(5)).Subscribe(_ =>
-            {
-                BackgroundImage?.Dispose();
-
-                PostOnUI(__ =>
-                {
-                    BackgroundImage = _deleteMe % 2 == 0
-                        ? new BackgroundImageViewModel("https://i.pinimg.com/originals/b9/fa/05/b9fa05f7aa2e9ded35d8e29ea79557a3.jpg") 
-                        : new BackgroundImageViewModel("https://wallpaperplay.com/walls/full/c/e/4/174967.jpg");
-                });
-                
-                _deleteMe++;
-            });
+            NextCommandText = "Next"; //localize
+            PreviousCommandText = "Previous"; //localize
+            SkipCommandText = "Skip"; //localize
         }
 
         #region Private Fields
+        private bool _showNextCommand;
+        private bool _showSkipCommand;
+        private bool _showPreviousCommand;
+        private StepViewModelBase _selectedStep;
         private BackgroundImageViewModel _backgroundImage;
+
         #endregion
     }
 }
---
+++ ActorWizard/ViewModels/Steps/StepViewModelBase.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows.Input;
+using Actor.UI.Common;
+
+namespace ActorWizard.ViewModels.Steps
+{
+    public abstract class StepViewModelBase : ViewModelBase
+    {
+        public int StepId { get; }
+        public string StepTitle { get; }
+
+        protected StepViewModelBase(int stepId, string stepTitle)
+        {
+            StepId = stepId;
+            StepTitle = stepTitle; // Localize
+        }
+    }
+}
